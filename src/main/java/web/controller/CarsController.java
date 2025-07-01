fcb0ca0446dd1a867f41b3c6b2ce38f079d4a1f6
Alexandr K <110187179+alexxxandrdrdr@users.noreply.github.com>
2025-07-01 20:18:28 +0300

    refactor: new layer 'Service' & code style

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,14 +1,14 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import web.model.Car;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarServiceImpl;
 
-import java.util.ArrayList;
+
 
 @Controller
 public class CarsController {
